ced72b706f0a734a9d30c28ca23d0dbd318cae50
EIdriss <idriss.e@mpwb.tech>
2026-06-27 15:48:12 +0100

    Feat / Add Pres2 instanciation dynamique

---
+++ src/main/java/net/idriss/pres/Pres2.java
@@ -0,0 +1,25 @@
+package net.idriss.pres;
+
+import net.idriss.dao.IDao;
+import net.idriss.metier.IMetier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
+
+public class Pres2 {
+    static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Scanner scanner = new Scanner(new File("config.txt"));
+
+        String daoClassName = scanner.nextLine();
+        Class<?> cDao = Class.forName(daoClassName);
+        IDao dao = (IDao) cDao.newInstance();
+
+        String metierClassName = scanner.nextLine();
+        Class<?> cMetier = Class.forName(metierClassName);
+        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+        System.out.println("Res = " + metier.calcul());
+    }
+}
